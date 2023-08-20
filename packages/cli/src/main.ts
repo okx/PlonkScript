35fbd2a3978dd025f53ece5fb06d04516ce81d6d
Icer <liangshuangde@163.com>
2023-08-20 21:43:13 +0800

    fix parser

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -16,7 +16,7 @@ content = content.replaceAll(/\)/g, "]");
 content = content.replace(/(['"])?([\w\d]+)(['"])?:\s*/g, '"$2": ');
 content = content.replace(/:\s*([\w\d\.]+)/g, ': "$1"');
 content = content.replace(/,(\s*[\}\]])/g, '$1');
-content = content.replace(/(0x[\d]+)/g, '"$1"');
+content = content.replace(/(0x[\da-f]+)/g, '"$1"');
 content = content.replace(/^(\s*)([\w\d]+)/gm, '$1"$2"');
 
 // console.log(content);
