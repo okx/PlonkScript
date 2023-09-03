12732cb953a9b3c357e2e921db4a9ccd6aff8c66
Icer <liangshuangde@163.com>
2023-09-03 11:45:56 +0800

    protect string for special character inside

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -3,8 +3,18 @@ import fs from "fs";
 
 let content = fs.readFileSync("src/input.rust", "utf8");
 
+// protect original string
+const pairs: Record<string, string> = {};
+const reString = /".*[\{\}\[\]\(\)]+.*"/g;
+const matches = content.matchAll(reString);
+for (const m of matches) {
+  const rnd = `"${Math.random()}"`;
+  pairs[rnd] = m[0];
+  content = content.replaceAll(m[0], rnd);
+}
+
 // convert `columns` set to array
-content = content.replaceAll(/(columns: )\{(\n(?: {16}.*\n)+)( {12}})/g, '$1[$2            ]');
+content = content.replaceAll(/(columns: )\{(\n(?: {16}.*\n)+)( {12}})/g, "$1[$2            ]");
 
 content = content.replaceAll(/({\s*)([^:]*)\s*\(/g, '$1"$2": (');
 
@@ -17,9 +27,17 @@ content = content.replaceAll(/\)/g, "]");
 
 content = content.replace(/(['"])?([\w\d]+)(['"])?:\s*/g, '"$2": ');
 content = content.replace(/:\s*([\w\d\.]+)/g, ': "$1"');
-content = content.replace(/,(\s*[\}\]])/g, '$1');
+content = content.replace(/,(\s*[\}\]])/g, "$1");
 content = content.replace(/(0x[\da-f]+)/g, '"$1"');
 content = content.replace(/^(\s*)([\w\d]+)/gm, '$1"$2"');
+
+// restore protected string
+for (const key in pairs) {
+  if (Object.prototype.hasOwnProperty.call(pairs, key)) {
+    const val = pairs[key];
+    content = content.replaceAll(key, val);
+  }
+}
 
 // console.log(content);
 fs.writeFileSync("../ui/src/assets/output.json", content);
