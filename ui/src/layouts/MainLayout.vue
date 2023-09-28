97ecff0f750f11b8c4a452daa9cbdbf84c8ab121
Shuang Liang (Icer) <shuang.liang@okg.com>
2023-09-28 15:27:01 +0800

    try transpiler

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -11,9 +11,7 @@
           @click="toggleLeftDrawer"
         /> -->
 
-        <q-toolbar-title>
-          Halo2 Visualizer
-        </q-toolbar-title>
+        <q-toolbar-title> Halo2 Visualizer </q-toolbar-title>
 
         <div>Demo Version</div>
       </q-toolbar>
@@ -46,6 +44,57 @@
 </template>
 
 <script setup lang="ts">
+// const rust = import('../transpiler/wasm_binding');
+
+// rust.then((m) => m.greet("hello")).catch(console.error);
+import init, { greet, try_run } from '../transpiler';
+
+async function run() {
+  await init();
+  greet('hello');
+  let code = `
+gate add(a, b, c, s) {
+    s <| a + b - c;
+}
+
+let N = 10;
+
+public input in1;
+public input in2;
+public output out;
+
+private advice a;
+private advice b;
+private advice c;
+
+public selector s;
+
+add(a, b, c, s);
+
+a[0] <== in1;
+b[0] <== in2;
+
+for i in 0..N {
+    if (i > 0) {
+        a[i] <== b[i - 1];
+        b[i] <== c[i - 1];
+    }
+    c[i] <== a[i] + b[i];
+    s[i] <-- enable;
+}
+
+out <== c[N-1];
+`;
+  const result = try_run({
+    k: 4,
+    code,
+    inputs: { in1: '1', in2: '1' },
+  });
+  console.log('result:', result);
+}
+
+run();
+
 // import { ref } from 'vue';
 // import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
 
